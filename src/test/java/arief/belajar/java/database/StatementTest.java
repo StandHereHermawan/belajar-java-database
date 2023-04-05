fa2226f8c2447e84317593fb537a3974c5a2c9c3
StandHereHermawan <nolifegansaya@gmail.com>
2023-04-05 15:42:10 +0700

    Statement SQL Unit Test

---
+++ src/test/java/arief/belajar/java/database/StatementTest.java
@@ -0,0 +1,20 @@
+package arief.belajar.java.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StatementTest {
+    @Test
+    void testCreateStatement() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+        Statement statement = connection.createStatement();
+
+        statement.close();
+
+        connection.close();
+    }
+}
