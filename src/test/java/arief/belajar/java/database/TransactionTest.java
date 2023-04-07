80ffc10a94aaec9800dd4ebd08610f9bdc57c96a
StandHereHermawan <nolifegansaya@gmail.com>
2023-04-07 20:16:28 +0700

    Database Transaction Unit Test

---
+++ src/test/java/arief/belajar/java/database/TransactionTest.java
@@ -0,0 +1,27 @@
+package arief.belajar.java.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TransactionTest {
+    @Test
+    void testDatabaseTransaction() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        connection.setAutoCommit(false);
+
+        String sql = "insert into comments(email, comment) values (?,?)";
+        for (int i = 0; i < 100; i++) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "hermawan@test.com");
+            preparedStatement.setString(2, "hi");
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        }
+
+        connection.commit();
+        connection.close();
+    }
+}
