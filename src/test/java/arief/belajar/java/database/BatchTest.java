3b6df82b491df37aedd94081ef3ce231529c9090
StandHereHermawan <nolifegansaya@gmail.com>
2023-04-07 16:04:52 +0700

    Batch Unit Test

---
+++ src/test/java/arief/belajar/java/database/BatchTest.java
@@ -0,0 +1,46 @@
+package arief.belajar.java.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BatchTest {
+    @Test
+    void testBatchStatement() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+
+        String sql = "insert into comments(email, comment) values ('arief@test.com','hi')";
+
+        for (int i = 0; i < 1000; i++) {
+            statement.addBatch(sql);
+        }
+
+        statement.executeBatch();
+
+        statement.close();
+        connection.close();
+    }
+
+    @Test
+    void testBatchPreparedStatement() throws SQLException{
+        Connection connection =ConnectionUtil.getDataSource().getConnection();
+        String sql = "insert into comments(email, comment) values (?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        for (int i = 0; i < 1000; i++) {
+            preparedStatement.clearParameters();
+            preparedStatement.setString(1,"karditya@test.com");
+            preparedStatement.setString(2,"hi");
+            preparedStatement.addBatch();
+        }
+
+        preparedStatement.executeBatch();
+
+        preparedStatement.close();
+        connection.close();
+    }
+}
